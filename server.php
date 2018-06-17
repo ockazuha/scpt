e22f01222c20f11886fe05a50b914922741ac3c4
ockazuha <ockazuha@gmail.com>
2018-06-17 20:11:27 +0400

    Доб. уведомление о завершении работы сервера из-за ошибки

--- server.php
+++ server.php
@@ -13,4 +13,9 @@ set_time_limit(0);
 ob_end_clean();
 cfg('is_call_500error', false);
 
-new MySocket(cfg('socket')['server_addr']);
+try {
+    new MySocket(cfg('socket')['server_addr']);
+} catch (Base_Exception $e) {
+    echo "An error occurred! The work has been completed.\n";
+    throw $e;
+}
